60ea8c7f2a18b5893db4e02167f3147f04cb1790
Oleksandr Nychyporchuk <alexnychyporchuk@gmail.com>
2022-05-30 15:48:30 +0300

    adding table

--- dist/all.js
+++ dist/all.js
@@ -53,3 +53,54 @@ function close_popup(){
     document.getElementById("shadow").style.display="none";
     document.getElementById("popup").style.display="none"; 
 }
+
+
+// api url
+const api_url ="https://jsonplaceholder.typicode.com/users";
+
+// Defining async function
+async function getapi(url) {
+	
+	// Storing response
+	const response = await fetch(url);
+	
+	// Storing data in form of JSON
+	var data = await response.json();
+	console.log(data);
+	if (response) {
+		hideloader();
+	}
+	show(data);
+}
+// Calling that async function
+getapi(api_url);
+
+// Function to hide the loader
+function hideloader() {
+	document.getElementById('loading').style.display = 'none';
+}
+// Function to define innerHTML for HTML table
+function show(data) {
+	let tab =
+		`<tr>
+		<th>Name and Surname</th>
+		<th>E-Mail</th>
+		<th>Address</th>
+		<th>Phone</th>
+    <th>Company Name</th>
+		</tr>`;
+	
+	// Loop to access all rows
+	for (let r of data) {
+    
+		tab += `<tr>
+	<td>${r.name} </td>
+	<td>${r.email}</td>
+	<td>${r.address.city} ${r.address.street} ${r.address.suite}</td>
+	<td>${r.phone}</td>		
+  <td>${r.company.name}</td>	
+</tr>`;
+	}
+	// Setting innerHTML as tab variable
+	document.getElementById("employees").innerHTML = tab;
+}
